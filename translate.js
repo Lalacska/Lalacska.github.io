c9eb3c2fd7ecfa197f49988e7124f7c569d4b6ac
Lalacska <87847275+Lalacska@users.noreply.github.com>
2024-04-04 14:54:03 +0200

    Work
    Added flags and links
    Started to add project section

--- translate.js
+++ translate.js
@@ -23,22 +23,42 @@ function setLanguage(language) {
 function switchLang(lang) {
     console.log("Switching language to: ", lang);
     for (let key in translations[lang]) {
-       document.getElementById(key).innerHTML = translations[lang][key];
+        document.getElementById(key).innerHTML = translations[lang][key];
     }
- }
+}
 
 // Add event listeners to language links
-document.getElementById('danish').addEventListener('click', function(e) {
+document.getElementById('danish').addEventListener('click', function (e) {
     e.preventDefault(); // Prevent default anchor behavior
     switchLang('danish');
 });
 
-document.getElementById('english').addEventListener('click', function(e) {
+document.getElementById('english').addEventListener('click', function (e) {
     e.preventDefault(); // Prevent default anchor behavior
     switchLang('english');
 });
 
-document.getElementById('hungarian').addEventListener('click', function(e) {
+document.getElementById('hungarian').addEventListener('click', function (e) {
     e.preventDefault(); // Prevent default anchor behavior
     switchLang('hungarian');
+});
+
+document.getElementById("LinkedIn").addEventListener("click", function () {
+    window.open("https://www.linkedin.com/in/laura-mezei-a16a831a7/", "_blank");
+});
+
+document.getElementById("GithubAccont").addEventListener("click", function () {
+    window.open("https://github.com/Lalacska?tab=overview&from=2022-10-01&to=2022-10-31",  "_blank");
+});
+
+document.getElementById("Flags").addEventListener("click", function () {
+    window.open("https://www.freeflagicons.com/", "_blank");
+});
+
+document.getElementById("Inspiration").addEventListener("click", function () {
+    window.open("https://www.pascalvangemert.nl/", "_blank");
+});
+
+document.getElementById("TicketToRide").addEventListener("click", function () {
+    window.open("https://github.com/Lalacska/Ticket-to-Ride", "_blank");
 });
